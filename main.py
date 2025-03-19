783ff63bc4623220f0bd22c034610329f38947cd
Christian Rosales <crimiro05@gmail.com>
2025-03-19 12:36:03 -0600

    Endpoint for uploading files and saving them temporarily

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 def message():
     return {"message": "Hello, World!"}
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = f"/tmp/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return JSONResponse(content={"filename": file.filename, "content_type": file.content_type})
